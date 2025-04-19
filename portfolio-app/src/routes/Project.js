4277e26ccd9541b4e598a71af3a28cf7d66a151b
Aditya Prabhu <90486414+adityaprabhu16@users.noreply.github.com>
2025-04-18 19:06:53 -0500

    removed unused variable

--- portfolio-app/src/routes/Project.js
+++ portfolio-app/src/routes/Project.js
@@ -3,7 +3,6 @@ import React from 'react'
 import Navbar from "../components/Navbar.js";
 import Footer from "../components/Footer.js";
 import HeroImg2 from '../components/HeroImg2.js';
-import PricingCard from "../components/PricingCard.js";
 import Work from "../components/Work.js";
 
 const Project = () => {
